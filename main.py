6e56f19ea46a815844a5bb85c91197c6782f8c08
WingkySky <37488098@qq.com>
2025-01-16 16:21:11 +0800

    修正后台日志弹出问题

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
 """
-Excel合并工具主程序
+程序入口模块
 """
+import os
+import sys
+import platform
+
+# 在 macOS 上重定向系统日志
+if platform.system() == 'Darwin':  # Darwin 是 macOS 的系统名
+    # 将标准错误输出重定向到 /dev/null
+    sys.stderr = open(os.devnull, 'w')
+
 import tkinter as tk
 import customtkinter as ctk
 from src.gui.main_window import ExcelMergerApp
 
 def main():
-    # 设置CustomTkinter的外观模式和默认颜色主题
-    ctk.set_appearance_mode("light")  # 模式选项: light, dark, system
-    ctk.set_default_color_theme("blue")  # 主题选项: blue, dark-blue, green
+    """主函数"""
+    # 设置默认主题
+    ctk.set_appearance_mode("system")
+    ctk.set_default_color_theme("blue")
     
     # 创建主窗口
-    root = tk.Tk()
+    root = ctk.CTk()
     app = ExcelMergerApp(root)
     root.mainloop()
 
